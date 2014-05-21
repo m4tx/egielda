66722b2053239978e07fce4292774c35ce81962b
m4tx <m4tx@m4tx.pl>
2014-05-21 23:36:45 +0200

    First version of models

--- shared/models.py
+++ shared/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    student_class = models.CharField(max_length=2)
+    phone_number = models.CharField(max_length=9)
+
+class BookType(models.Model):
+    publishing_house = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    issue = models.IntegerField(default=1)
+    issue_year = models.IntegerField(default=-1)
+    price = models.IntegerField()
+
+class Book(models.Model):
+    type = models.ForeignKey(BookType)
+    owner = models.ForeignKey(Student)
+    physical = models.BooleanField()
+    """Is the book physically available for buying"""
+    sold = models.BooleanField()
+
+class Purchase(models.Model):
+    book = models.ForeignKey(Book)
+    date = models.DateField()
+    purchaser = models.ForeignKey(Student)
