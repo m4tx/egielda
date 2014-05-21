5953c19789d5be0a3618f0e36de79f39c3a434d9
m4tx <m4tx@m4tx.pl>
2014-05-21 22:12:03 +0200

    Add simple page template (/manage/books)

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from books import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index')
 ]
--- egielda/settings.py
+++ egielda/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shared',
+    'books',
 )
 
 MIDDLEWARE_CLASSES = (
