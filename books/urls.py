45a6f282a1f54e157c4c5209df583f5a74bdc7c3
m4tx <m4tx@m4tx.pl>
2014-05-22 16:40:25 +0200

    Add "Add", "Edit" and "Remove book" pages stubs

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from books import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^add/$', 'books.views.add_book'),
+    url(r'^edit/([0-9]+)/$', 'books.views.edit_book'),
+    url(r'^remove/([0-9]+)/$', 'books.views.remove_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,21 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+
 from shared.models import BookType
 
 
 def index(request):
     book_list = BookType.objects.all()
     return render(request, 'books/index.html', {'book_list': book_list})
+
+
+def add_book(request):
+    return HttpResponse("Hello world!")
+
+
+def edit_book(request, book_id):
+    return HttpResponse("Hello world!")
+
+
+def remove_book(request, book_id):
+    return HttpResponse("Hello world!")
