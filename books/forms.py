eb57c1498c683fca56d991e98fc6945ba6a72613
m4tx <m4tx@m4tx.pl>
2014-05-28 17:56:38 +0200

    Improve Price field behavior

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.translation import ugettext as _
 
+from egielda import settings
+
 
 class BookForm(forms.Form):
     publisher = forms.CharField(help_text=_("Publisher"), max_length=150,
@@ -11,5 +13,5 @@ class BookForm(forms.Form):
                                widget=forms.NumberInput(attrs={'required': 'required'}))
     issue_year = forms.IntegerField(help_text=_("Issue year"), min_value=1900, initial=2000, max_value=2100,
                                     widget=forms.NumberInput(attrs={'required': 'required'}))
-    price = forms.DecimalField(help_text=_("Price"), max_digits=5, decimal_places=2,
+    price = forms.IntegerField(help_text=_("Price (%s)") % getattr(settings, 'CURRENCY', 'USD'), initial=20,
                                widget=forms.NumberInput(attrs={'required': 'required', 'step': '0.5'}))
