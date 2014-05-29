9a2378b0ac9e01df8418897f76bee72113d07819
m4tx <m4tx@m4tx.pl>
2014-05-29 18:00:06 +0200

    Remove edition and publication year fields from Model

--- books/forms.py
+++ books/forms.py
@@ -1,17 +1,29 @@
 from django import forms
+from django.forms import ModelForm
 from django.utils.translation import ugettext as _
+from books.models import BookType
 
 from egielda import settings
 
 
-class BookForm(forms.Form):
-    publisher = forms.CharField(help_text=_("Publisher"), max_length=150,
-                                widget=forms.TextInput(attrs={'required': 'required'}))
-    title = forms.CharField(help_text=_("Title"), max_length=150,
-                            widget=forms.TextInput(attrs={'required': 'required'}))
-    edition = forms.IntegerField(help_text=_("Edition"), min_value=1, initial=1,
-                                 widget=forms.NumberInput(attrs={'required': 'required'}))
-    publication_year = forms.IntegerField(help_text=_("Publication year"), min_value=1900, initial=2000, max_value=2100,
-                                          widget=forms.NumberInput(attrs={'required': 'required'}))
-    price = forms.IntegerField(help_text=_("Price (%s)") % getattr(settings, 'CURRENCY', 'USD'), initial=20,
-                               widget=forms.NumberInput(attrs={'required': 'required', 'step': '0.5'}))
+class BookForm(ModelForm):
+    class Meta:
+        model = BookType
+        fields = ['publisher', 'title', 'price']
+        initial = {'publisher': 'gowno'}
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['price'].initial = 20
+
+
+    # publisher = forms.CharField(help_text=_("Publisher"), max_length=150,
+    #                             widget=forms.TextInput(attrs={'required': 'required'}))
+    # title = forms.CharField(help_text=_("Title"), max_length=150,
+    #                         widget=forms.TextInput(attrs={'required': 'required'}))
+    # edition = forms.IntegerField(help_text=_("Edition"), min_value=1, initial=1,
+    #                              widget=forms.NumberInput(attrs={'required': 'required'}))
+    # publication_year = forms.IntegerField(help_text=_("Publication year"), min_value=1900, initial=2000, max_value=2100,
+    #                                       widget=forms.NumberInput(attrs={'required': 'required'}))
+    # price = forms.IntegerField(help_text=_("Price (%s)") % getattr(settings, 'CURRENCY', 'USD'), initial=20,
+    #                            widget=forms.NumberInput(attrs={'required': 'required', 'step': '0.5'}))
--- books/models.py
+++ books/models.py
@@ -7,16 +7,11 @@ from egielda import settings
 class BookType(models.Model):
     publisher = models.CharField(max_length=150)
     title = models.CharField(max_length=150)
-    edition = models.IntegerField()
-    publication_year = models.IntegerField()
     price = models.IntegerField()
 
     def price_string(self):
         return "%(price).2f%(currency)s" % {'price': (self.price / 100),
                                             'currency': getattr(settings, 'CURRENCY', 'USD')}
-
-    def edition_string(self):
-        return _("Edition %(edition)d, %(year)d") % {'edition': self.edition, 'year': self.publication_year}
 
     def __str__(self):
         return _(
