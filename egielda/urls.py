23bf8168dedc37443530f962d9ff27e6623c8ce7
m4tx <m4tx@m4tx.pl>
2014-05-30 18:37:13 +0200

    Add Categories app stub

--- egielda/urls.py
+++ egielda/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^manage/books/', include('books.urls')),
+    url(r'^manage/categories/', include('categories.urls')),
 )
