efabfa4dc4b6d6edd648261a42de975e4f3aae02
m4tx <m4tx@m4tx.pl>
2014-06-06 00:48:58 +0200

    Move book chooser code to separate file
    gh-6

--- egielda/urls.py
+++ egielda/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+js_info_dict = {
+    'packages': ('egielda',),
+}
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'egielda.views.home', name='home'),
@@ -12,4 +16,5 @@ urlpatterns = patterns('',
     url(r'^manage/books/', include('books.urls')),
     url(r'^manage/categories/', include('categories.urls')),
     url(r'^sell/', include('sell.urls')),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 )
---
+++ static/js/bookchooser.js
@@ -0,0 +1,101 @@
+$('.btn-add-book').on('click', function() {
+    var tr = $(this).closest($('tr'));
+    var book = new ExistingBook(tr.data('pk'));
+    chosenBooks.push(book);
+    addExistingBook(tr, book);
+});
+$('#btn-add-new-book').on('click', function () {
+    addNewBook();
+});
+$('button#btn-next,button#btn-back').on('click', function() {
+    $('form').append($('<input type="hidden" name="book_data">').attr('value', JSON.stringify(chosenBooks)))
+        .append($('<input type="hidden" name="' + this.id + '">'))
+        .submit();
+});
+
+function ExistingBook(pk) {
+    this.pk = pk;
+}
+
+function NewBook() {
+}
+
+function addRetrievedBooks() {
+    for (var book in chosenBooks) {
+        if (chosenBooks[book].pk != undefined) {
+            addExistingBook($('#bookList').find('table tr[data-pk="' + chosenBooks[book].pk + '"]'));
+        } else {
+            addBookTr(createNewBookTr(chosenBooks[book]));
+        }
+    }
+}
+
+function addExistingBook(tr, book) {
+    var ctr = tr.clone();
+    tr.addClass('hidden');
+    $('td:last-child', ctr).remove();
+
+    addBookTr(ctr, book);
+}
+
+function addNewBook() {
+    var ids = ['isbn', 'publisher', 'title', 'publication_year', 'price'];
+    var vals = [];
+
+    var book = new NewBook();
+    var add = false;
+    for (var prid in ids) {
+        var input = $('input[name="' + ids[prid] + '"]');
+        book[ids[prid]] = input.val();
+        input.val("");
+
+        if (book[ids[prid]] != "") {
+            add = true;
+        }
+        vals.push(book[ids[prid]]);
+    }
+
+    if (add) {
+        chosenBooks.push(book);
+        addBookTr(createNewBookTr(vals), book);
+    }
+}
+
+function createNewBookTr(vals) {
+    var tr = $('<tr/>');
+
+    for (var id in vals) {
+        tr = tr.append($('<td/>').text(vals[id]));
+    }
+
+    return tr;
+}
+
+function addBookTr(tr, book) {
+    var button = $('<button class="btn btn-xs btn-link btn-remove-book"><span class="glyphicon glyphicon-remove"></span> ' + gettext("Remove") + '</button>');
+    button.on('click', function() {
+        removeBook($('#chosen-books-list').find('button.btn-remove-book').index(this))
+    });
+    tr.append($('<td/>').append(button));
+
+    var chosenTable = $('#chosen-books-list');
+    chosenTable.removeClass('hidden');
+    chosenTable.append(tr);
+}
+
+function removeBook(id) {
+    var book = chosenBooks[id];
+    if (book.pk != undefined) {
+        $('#bookList').find('table tr[data-pk="' + book.pk + '"]').removeClass('hidden');
+    }
+
+    var chosenBooksList = $('#chosen-books-list');
+    chosenBooks.splice(id, 1);
+    chosenBooksList.find('tbody tr:eq(' + id + ')').remove();
+
+    if (chosenBooks.length == 0) {
+        chosenBooksList.addClass('hidden');
+    }
+}
+
+addRetrievedBooks();
