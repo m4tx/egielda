141e8303fe8f1d6fe554770d7480ef50797d4735
m4tx <m4tx@m4tx.pl>
2014-06-17 18:22:52 +0200

    Improve ISBN field in Book Type form
    - Add required, pattern (to allow only numbers and dashes in HTML5-supporting browsers) and title properties
    - Remove a bit of redundant code

--- books/forms.py
+++ books/forms.py
@@ -7,21 +7,21 @@ from egielda import settings
 
 
 class BookForm(ModelForm):
-    # Different max_length than in model (to allow dividers in ISBN number)
-    isbn = forms.CharField(max_length=20, label=_("ISBN"))
+    # Different max_length than in model (to allow dashes in ISBN)
+    isbn = forms.CharField(max_length=20, label=_("ISBN"),
+                           widget=forms.TextInput(
+                               attrs={'required': 'required', 'pattern': '[0-9-]+', 'title': 'ISBN number'}))
 
     class Meta:
         model = BookType
         fields = ['isbn', 'publisher', 'title', 'publication_year', 'price']
         labels = {
-            'isbn': _("ISBN"),
             'publisher': _("Publisher"),
             'title': _("Title"),
             'publication_year': _("Publication year"),
             'price': _("Price (%s)") % getattr(settings, 'CURRENCY', 'USD'),
         }
         widgets = {
-            'isbn': forms.TextInput(attrs={'required': 'required'}),
             'publisher': forms.TextInput(attrs={'required': 'required'}),
             'title': forms.TextInput(attrs={'required': 'required'}),
             'publication_year': forms.NumberInput(attrs={'required': 'required', 'min': '1900', 'max': '2100'}),
