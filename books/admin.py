237cc8d4cd1d568bc4ff22c7281a32ca3a99c3d0
m4tx <m4tx@m4tx.pl>
2014-07-01 20:33:23 +0200

    Move BookType model registration to books/admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import BookType
+
+
+admin.site.register(BookType)
--- common/admin.py
+++ common/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from common.models import AppUser, BookType, Book, Purchase
+
+from common.models import AppUser, Book, Purchase
+
 
 admin.site.register(AppUser)
-admin.site.register(BookType)
 admin.site.register(Book)
 admin.site.register(Purchase)
