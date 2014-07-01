10cb2f97b599afae7444139d3e3166a2c5351c92
m4tx <m4tx@m4tx.pl>
2014-07-01 21:07:43 +0200

    Fix Categories app URLs

--- categories/urls.py
+++ categories/urls.py
@@ -5,7 +5,7 @@ from categories import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^add$', views.add_category),
-    url(r'^edit/([0-9]+)$', views.edit_category),
-    url(r'^remove/([0-9,]+)$', views.remove_category),
+    url(r'^add/$', views.add_category),
+    url(r'^edit/([0-9]+)/$', views.edit_category),
+    url(r'^remove/([0-9,]+)/$', views.remove_category),
 ]
