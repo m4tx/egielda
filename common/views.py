2995345746287b06bc737afb95c00b9ae27e5d28
m4tx <m4tx@m4tx.pl>
2014-07-15 21:03:56 +0200

    Fix formatting

--- common/views.py
+++ common/views.py
@@ -106,6 +106,7 @@ class BookChooserWizard:
             else:
                 return render(request, 'book_chooser_wizard/summary.html',
                               {'page_title': self.page_title, 'personal_data': request.session['personal_data'],
-                               'chosen_book_list': sorted(self.get_book_list(book_list), key=lambda x: x.title.lower())})
+                               'chosen_book_list': sorted(self.get_book_list(book_list),
+                                                          key=lambda x: x.title.lower())})
         except ValueError:
             return HttpResponseBadRequest()
