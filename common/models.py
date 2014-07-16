2c700498c83da498675bd34337591bcb8b357330
m4tx <m4tx@m4tx.pl>
2014-07-16 19:52:32 +0200

    Fix AttributeError when adding Purchases via admin panel

--- common/models.py
+++ common/models.py
@@ -37,7 +37,7 @@ class Purchase(models.Model):
 
     def __str__(self):
         return _("%(book)s from %(owner)s sold to %(purchaser)s at %(date)s" %
-                 {'book': self.book.type, 'owner': self.book.owner, 'purchaser': self.purchaser, 'date': self.date})
+                 {'book': self.book.book_type, 'owner': self.book.owner, 'purchaser': self.purchaser, 'date': self.date})
 
 class Setting(models.Model):
     name = models.CharField(max_length=100)
