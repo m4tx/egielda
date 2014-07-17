aff512829069405bad306660d0f84ba0da973e25
tmp12342 <tmp12342@gmail.com>
2014-07-17 15:43:40 +0200

    Install Django Debug Toolbar

--- egielda/settings.py
+++ egielda/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar.apps.DebugToolbarConfig',
     'widget_tweaks',
     'common',
     'books',
