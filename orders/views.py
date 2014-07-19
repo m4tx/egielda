1c5c46201add431debcbfccd61be722c54825af1
m4tx <m4tx@m4tx.pl>
2014-07-20 00:26:12 +0200

    Add simple Order displaying

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from orders.models import Order
 
 
 def not_executed(request):
-    return render(request, 'orders/not_executed.html')
+    orders = Order.objects.filter(valid_until__gt=timezone.now())
+    return render(request, 'orders/not_executed.html', {'orders': orders})
 
 
 def outdated(request):
-    return render(request, 'orders/outdated.html')
+    orders = Order.objects.filter(valid_until__lte=timezone.now())
+    return render(request, 'orders/outdated.html', {'orders': orders})
 
 
 def executed(request):
