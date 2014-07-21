089cb02d54ce589be3f5eae5277a1ec6a9ea5e7b
m4tx <m4tx@m4tx.pl>
2014-07-21 14:38:16 +0200

    Add Order details view stub

--- orders/urls.py
+++ orders/urls.py
@@ -7,6 +7,7 @@ from orders import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url=reverse_lazy(views.not_executed)), name='orders.views.index'),
+    url(r'^([0-9]+)/$', views.order_details),
     url(r'^notexecuted/$', views.not_executed),
     url(r'^outdated/$', views.outdated),
     url(r'^executed/$', views.executed),
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 
 from orders.models import Order
+
+
+def order_details(request, order_pk):
+    return HttpResponse("Hello, world!")
 
 
 def not_executed(request):
