f4406d21546922363cd67f53d5697bc324306f2b
m4tx <m4tx@m4tx.pl>
2014-07-23 17:46:53 +0200

    Implement actual filtering (not) executed Orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
@@ -10,14 +12,23 @@ def order_details(request, order_pk):
 
 
 def not_executed(request):
-    orders = Order.objects.filter(valid_until__gt=timezone.now())
+    orders = get_orders().filter(valid_until__gt=timezone.now(), sold_count=0)
     return render(request, 'orders/not_executed.html', {'orders': orders})
 
 
 def outdated(request):
-    orders = Order.objects.filter(valid_until__lte=timezone.now())
+    orders = get_orders().filter(valid_until__lte=timezone.now(), sold_count=0)
     return render(request, 'orders/outdated.html', {'orders': orders})
 
 
 def executed(request):
-    return render(request, 'orders/executed.html')
+    orders = get_orders().exclude(sold_count=0)
+    return render(request, 'orders/executed.html', {'orders': orders})
+
+
+def get_orders() -> QuerySet:
+    """
+    The function returns QuerySet of Order model with all necessary values for displaying also selected/prefetched.
+    :return: the QuerySet of Order model
+    """
+    return Order.objects.select_related('user').prefetch_related('books').annotate(sold_count=Sum('books__sold'))
