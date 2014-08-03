7e733c79dfd354b8689af4961547d97b0f948326
tmp12342 <tmp12342@gmail.com>
2014-08-03 20:37:42 +0200

    Remove unnecessary setting

--- egielda/settings.py
+++ egielda/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
 
 SITE_NAME = "e-Giełda"
 LOGIN_REDIRECT_URL = 'egielda.views.home'
-LOGIN_URL = '/accounts/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
