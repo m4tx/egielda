50822ddf4a6ab900819bafdd17ae48db10f8e899
tmp12342 <tmp12342@gmail.com>
2014-08-09 09:32:33 +0200

    Optimize creating groups

--- egielda/__init__.py
+++ egielda/__init__.py
@@ -1,73 +1,82 @@
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group, Permission, ContentType
 from django.db.models.signals import post_migrate
 
 
 def create_groups(sender, **kwargs):
-    moderator_group = Group.objects.get_or_create(name='moderator')[0]
-    admin_group = Group.objects.get_or_create(name='admin')[0]
-    sysadmin_group = Group.objects.get_or_create(name='sysadmin')[0]
+    if ContentType.objects.filter(name='dummy permissions').count() > 0:
 
-    moderator_permissions = [
-        'view_books_index',
-        'view_books_add_book',
-        'view_books_edit_book',
-        'view_books_book_details',
+        moderator_group = Group.objects.get_or_create(name='moderator')[0]
+        admin_group = Group.objects.get_or_create(name='admin')[0]
+        sysadmin_group = Group.objects.get_or_create(name='sysadmin')[0]
 
-        'view_categories_index',
-        'view_categories_list_books',
+        if moderator_group.permissions.count() > 0 and admin_group.permissions.count() > 0 and \
+                sysadmin_group.permissions.count() > 0:
+            return
 
-        'view_managemenu_index',
+        if kwargs['verbosity'] != 0:
+            print("Creating groups...")
 
-        'view_orders_index',
-        'view_orders_order_details',
-        'view_orders_not_executed',
-        'view_orders_outdated',
-        'view_orders_executed',
-        'view_orders_execute',
-        'view_orders_execute_accept',
+        moderator_permissions = [
+            'view_books_index',
+            'view_books_add_book',
+            'view_books_edit_book',
+            'view_books_book_details',
 
-        'view_sellers_index',
-        'view_sellers_accept_books',
-        'view_sellers_accept_edit_book',
-        'view_sellers_remove_seller',
+            'view_categories_index',
+            'view_categories_list_books',
 
-        'view_stats_index',
-    ]
+            'view_managemenu_index',
 
-    moderator_permissions_objects = Permission.objects.filter(codename__in=moderator_permissions).exclude(
-        pk__in=[permission.pk for permission in moderator_group.permissions.all()])
-    moderator_permissions_objects_list = [o for o in moderator_permissions_objects]
+            'view_orders_index',
+            'view_orders_order_details',
+            'view_orders_not_executed',
+            'view_orders_outdated',
+            'view_orders_executed',
+            'view_orders_execute',
+            'view_orders_execute_accept',
 
-    moderator_group.permissions.add(*moderator_permissions_objects_list)
+            'view_sellers_index',
+            'view_sellers_accept_books',
+            'view_sellers_accept_edit_book',
+            'view_sellers_remove_seller',
 
-    admin_permissions = moderator_permissions + [
-        'view_books_remove_book',
+            'view_stats_index',
+        ]
 
-        'view_categories_add_category',
-        'view_categories_edit_category',
-        'view_categories_remove_category',
+        moderator_group.permissions.clear()
+        moderator_permissions_objects = Permission.objects.filter(codename__in=moderator_permissions)
+        moderator_permissions_objects_list = [o for o in moderator_permissions_objects]
 
-        'view_settings_index',
+        moderator_group.permissions.add(*moderator_permissions_objects_list)
 
-        'view_stats_books_sold',
-        'view_stats_users',
-        'view_stats_list_books',
-        'view_stats_books',
-    ]
+        admin_permissions = moderator_permissions + [
+            'view_books_remove_book',
 
-    admin_permissions_objects = Permission.objects.filter(codename__in=admin_permissions).exclude(
-        pk__in=[permission.pk for permission in admin_group.permissions.all()])
-    admin_permissions_objects_list = [o for o in admin_permissions_objects]
+            'view_categories_add_category',
+            'view_categories_edit_category',
+            'view_categories_remove_category',
 
-    admin_group.permissions.add(*admin_permissions_objects_list)
+            'view_settings_index',
 
-    sysadmin_permissions = admin_permissions
+            'view_stats_books_sold',
+            'view_stats_users',
+            'view_stats_list_books',
+            'view_stats_books',
+        ]
 
-    sysadmin_permissions_objects = Permission.objects.filter(codename__in=sysadmin_permissions).exclude(
-        pk__in=[permission.pk for permission in sysadmin_group.permissions.all()])
-    sysadmin_permissions_objects_list = [o for o in sysadmin_permissions_objects]
+        admin_group.permissions.clear()
+        admin_permissions_objects = Permission.objects.filter(codename__in=admin_permissions)
+        admin_permissions_objects_list = [o for o in admin_permissions_objects]
 
-    sysadmin_group.permissions.add(*sysadmin_permissions_objects_list)
+        admin_group.permissions.add(*admin_permissions_objects_list)
+
+        sysadmin_permissions = admin_permissions
+
+        sysadmin_group.permissions.clear()
+        sysadmin_permissions_objects = Permission.objects.filter(codename__in=sysadmin_permissions)
+        sysadmin_permissions_objects_list = [o for o in sysadmin_permissions_objects]
+
+        sysadmin_group.permissions.add(*sysadmin_permissions_objects_list)
 
 
 post_migrate.connect(create_groups)
