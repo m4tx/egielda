fd4c0d040039c2bc63df68d919d949d389aca395
tmp12342 <tmp12342@gmail.com>
2014-08-15 19:48:07 +0200

    Humanize date in statistics

--- egielda/settings.py
+++ egielda/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
     'django.contrib.staticfiles',
     'widget_tweaks',
     'common',
