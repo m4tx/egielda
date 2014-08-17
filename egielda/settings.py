d32f0673d04cce53d30a6e71e0a6a181a3aee32d
m4tx <m4tx@m4tx.pl>
2014-08-17 11:39:19 +0200

    Clean-up settings file

--- egielda/settings.py
+++ egielda/settings.py
@@ -16,13 +16,10 @@ Django settings for egielda project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 from django.conf import global_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -31,19 +28,35 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Security-related settings
+DEBUG = True  # Don't run with True on production!
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
 SECRET_KEY = 'oi8%=kw9=ufj1g!jc1$ylxmvlk*7o8f=j_braal62(pj)z@m9f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-TEMPLATE_DEBUG = True
+# General settings
+SITE_NAME = "e-Giełda"
+STATIC_URL = '/static/'
 
-ALLOWED_HOSTS = []
+# Internationalization
+USE_I18N = True
+USE_L10N = True
+LANGUAGE_CODE = 'pl'
+CURRENCY = 'zł'
+
+TIME_ZONE = 'Europe/Warsaw'
+USE_TZ = True
 
 
-# Application definition
-
+# Other settings
 INSTALLED_APPS = (
     'egielda',
     'django.contrib.admin',
@@ -81,48 +94,16 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
 )
 
 LOCALE_PATHS = (
-    'locale',  # Need to set it here in order to share translations between apps
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "vendor"),
 )
 
 ROOT_URLCONF = 'egielda.urls'
 
 WSGI_APPLICATION = 'egielda.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-SITE_NAME = "e-Giełda"
 LOGIN_REDIRECT_URL = 'egielda.views.home'
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'pl'
-CURRENCY = 'zł'
-
-TIME_ZONE = 'Europe/Warsaw'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "vendor"),
-)
