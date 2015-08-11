799e36a2bdf875d1e4b1c37bd6b7567eaf309bfd
unknown <tmp12342@gmail.com>
2015-08-11 22:09:25 +0200

    Add cleaning for first and last name

--- authentication/forms.py
+++ authentication/forms.py
@@ -60,6 +60,12 @@ class UserDataForm(ModelForm):
 
         return username
 
+    def clean_first_name(self):
+        return self.cleaned_data['first_name'].strip()
+
+    def clean_last_name(self):
+        return self.cleaned_data['last_name'].strip()
+
     def clean_student_class(self):
         student_class = self.cleaned_data['student_class']
 
